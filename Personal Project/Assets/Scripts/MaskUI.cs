5f88acdbec77177af54d55e8e7881a306e95125d
nathanlin109 <nathanlin109@gmail.com>
2021-01-05 05:36:40 -0500

    Continued new way of masking UI

---
+++ Personal Project/Assets/Scripts/MaskUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MaskUI : MonoBehaviour
+{
+    // Fields
+    public bool isText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isText == false)
+        {
+            Material newMat = Instantiate(GetComponent<Image>().material);
+            newMat.renderQueue = 3002;
+            GetComponent<Image>().material = newMat;
+        }
+        else
+        {
+            Material newMat = Instantiate(GetComponent<TMP_Text>().fontMaterial);
+            newMat.renderQueue = 3002;
+            GetComponent<TMP_Text>().fontMaterial = newMat;
+        }
+    }
+}
