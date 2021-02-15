7ef7779c7fb63865cdcdb510fd812263a3acadc2
nathanlin109 <nathanlin109@gmail.com>
2021-02-15 10:02:40 -0500

    Update ParallaxEffect.cs

--- Personal Project/Assets/Scripts/ParallaxEffect.cs
+++ Personal Project/Assets/Scripts/ParallaxEffect.cs
@@ -9,18 +9,22 @@ public class ParallaxEffect : MonoBehaviour
     private GameObject platformManager;
     private float platformManagerSpeed;
     private Vector3 initalPos;
+    private Vector3 targetPos;
     public float parallaxMultiplier;
     private float textureUnitSizeX;
     private float scaledTextureUnitSizeX;
+    private Transform cameraTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         // Gets platform manager game object
         platformManager = GameObject.Find("PlatformManager");
+        cameraTransform = GameObject.Find("Main Camera").transform;
         
         // Gets initial position of layer. Will be used to implement infinite scrolling
-        initalPos = transform.position;
+        initalPos = transform.position - cameraTransform.position;
+        targetPos = new Vector3(initalPos.x, initalPos.y, initalPos.z);
 
         // Calculates size of sprite in units (as opposed to pixels)
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
@@ -38,13 +42,17 @@ public class ParallaxEffect : MonoBehaviour
         // Moves the parallax layer by a factor of the platform manager speed to create a parallax effect
         transform.Translate(Vector2.left * (platformManagerSpeed * parallaxMultiplier) * Time.deltaTime);
 
+        targetPos = cameraTransform.position + initalPos;
+
         // Implements infinite scrolling by moving layer by texture size in units
-        if (Mathf.Abs(transform.position.x - initalPos.x) >= scaledTextureUnitSizeX)
+        if (Mathf.Abs(transform.position.x - targetPos.x) >= scaledTextureUnitSizeX)
         {
             // Calculates offset to prevent jitter when resetting position
-            float offsetX = (Mathf.Abs(transform.position.x - initalPos.x) % scaledTextureUnitSizeX);
-            transform.position = new Vector3(transform.position.x + scaledTextureUnitSizeX + offsetX, transform.position.y, transform.position.z);
-
+            float offsetX = (Mathf.Abs(transform.position.x - targetPos.x) % scaledTextureUnitSizeX);
+            transform.position = new Vector3(
+                transform.position.x + scaledTextureUnitSizeX + offsetX,
+                transform.position.y,
+                transform.position.z);
         }
     }
 
@@ -52,6 +60,7 @@ public class ParallaxEffect : MonoBehaviour
     public void Reset()
     {
         // Resets layer's position to its starting position
-        transform.position = initalPos;
+        transform.position = initalPos + cameraTransform.position;
+        targetPos = initalPos;
     }
 }
